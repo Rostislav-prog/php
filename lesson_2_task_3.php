9245735967c9824c248a301dfd0abd81eca69fcb
Rostislav <khmelinin.r@gmail.com>
2022-03-01 15:26:38 +0900

    task_5

--- lesson_2_task_3.php
+++ lesson_2_task_3.php
@@ -1,16 +1,26 @@
 <?php
 
 $a = rand(1, 10);
-$b = rand(11, 20);
+$b = rand(0, 2);
 
-function arithmetic($var1, $var2)
+function sum($arg1, $arg2)
 {
-    $add = $var1 + $var2;
-    $sub = $var1 - $var2;
-    $mul = $var1 * $var2;
-    $dev = $var1 / $var2;
+    return $arg1 + $arg2;
+}
 
-    return "$add <br> $sub <br>  $mul <br>  $dev";
+function sub($arg1, $arg2)
+{
+    return $arg1 - $arg2;
+}
+
+function mul($arg1, $arg2)
+{
+    return $arg1 * $arg2;
+}
+
+function dev($arg1, $arg2)
+{
+    return ($arg2 === 0)? "на 0 делить нельзя" :  $arg1 / $arg2;
 }
 
 echo arithmetic($a, $b);
